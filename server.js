1dd103d8142c6300815b01a8c425961b8d3a47a2
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-06 16:33:25 +0100

    fix file server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ app.get("/", (request, response) => {
     response.send('<span style="color: #f00;"> Hello Word ! </span>');
 })
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log("listenin on port " + PORT);
-})
+const server = app.listen(process.env.PORT || 5000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+  });
