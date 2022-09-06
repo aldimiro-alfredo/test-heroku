95c827a06b298bfb744537e4fa324a7e7ce62727
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-06 15:14:32 +0100

    this commit fix name

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.get('/', (request, response) => {
     response.send('<span style="color: #f00;"> Hello Word ! </span>');
-
 })
 
 const PORT = process.env.PORT || 5000;
