a2cab7462d0ace69cc486223f9d3e9246cdc181b
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-07 14:17:30 +0100

    bulid files news

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const express = require('express'); 
-const app = express(); 
- 
-app.get('/', (req, res) => { 
-  res 
-    .status(200) 
-    .send(' Olá servidor está rodando ') 
-    .end(); 
-}); 
- 
-// Inicia o servidor 
-const PORT = process.env.PORT || 8080; 
-app.listen(PORT, () => { 
-  console.log(`App escutando na porta ${PORT}`); 
-  console.log('Pressione Ctrl+C para sair.'); 
-});
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
