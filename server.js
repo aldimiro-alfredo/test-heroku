89be714356d7a9a4cdaeac6c6bb205576192c516
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-07 12:20:12 +0100

    this commit test deploy

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-
-const express = require('express')
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-    res.send('Hello from Node.js!')
-})
-
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+const express = require('express'); 
+const app = express(); 
+ 
+app.get('/', (req, res) => { 
+  res 
+    .status(200) 
+    .send(' Olá servidor está rodando ') 
+    .end(); 
+}); 
+ 
+// Inicia o servidor 
+const PORT = process.env.PORT || 8080; 
+app.listen(PORT, () => { 
+  console.log(`App escutando na porta ${PORT}`); 
+  console.log('Pressione Ctrl+C para sair.'); 
+});
