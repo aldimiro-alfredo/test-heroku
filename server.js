cbbf97f6b94f9021ff06e30c555a49aa357eebb9
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-12 10:42:05 +0100

    run build prod/dist

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/angular-deploy'))
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/dist/angular-deploy(index.html');
+    res.sendFile(__dirname + '/dist/angular-deploy/index.html');
 })
 
 app.listen(PORT, () => {
