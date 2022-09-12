3803244c661375ce3de0292af95950156ee71e93
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-12 09:24:29 +0100

    change v to angularjs

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express()
-const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/angular-deploy'))
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/angular-deploy(index.html');
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log("Servidor iniciado na porta " + PORT);
 })
