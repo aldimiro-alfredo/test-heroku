7f0542baaaaa67f0f17eeb23404911b6c5844f5a
aldimiro alfredo <aldimiroalfredo@gmail.com>
2022-09-12 10:55:19 +0100

    enable prod v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { enableProdMode } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
 @NgModule({
   declarations: [
     AppComponent
